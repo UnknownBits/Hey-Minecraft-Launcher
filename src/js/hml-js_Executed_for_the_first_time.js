3acf75bfc8f1bf2e8e6236e70cc25e023f834d02
xiaozhenee25 <1409178687@qq.com>
2025-02-03 21:31:15 +0800

    给软件刚启动添加了动画

--- src/js/hml-js_Executed_for_the_first_time.js
+++ src/js/hml-js_Executed_for_the_first_time.js
@@ -1,4 +1,10 @@
 // 刚启动的首次执行
 
+var hml_js_Animation_speed = "1"
+var hml_js_css_js = document.getElementById('hml_js_qt_1');//全页面
+
+    hml_js_css_js.style.display = ""
+    hml_js_css_js.style.animation = 'hml_css_Dim_animations2 ' + hml_js_Animation_speed + 's linear 0s 1 normal forwards'; //执行动画-反
+
 var hml_js_css_js = document.getElementById('hml_js_fg2');//分割2
     hml_js_css_js.style.display = 'none'; //隐藏
